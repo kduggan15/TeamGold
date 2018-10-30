6581ff60a2477eb8d550e5243f38e5674c4fbf17
Bon Liu <bon@Bons-MacBook-Pro.local>
2018-10-30 10:52:04 -0400

    change password to run test

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '12345678',
+  password : 'password',
   database : 'Gold'
 });
 
